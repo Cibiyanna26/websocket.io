6189a96ce675ada864223554238a699099b98618
Cibiyanna <pkcibiyanna@gmail.com>
2024-04-07 12:35:38 +0530

    baordcasting user connected and disconnect

--- server/index.js
+++ server/index.js
@@ -1,17 +1,27 @@
-const http = require('http');
 const express = require('express');
-const socket = require('socket.io');
-
 const app = express();
-const server = http.createServer(app,(req,res)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end('Helloworld\n')
+const http = require('http');
+const server = http.createServer(app);
+
+const {Server} = require('socket.io')
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+    io.emit('notification','new message')
 });
-
-const io = socketIo(server);
-
-
-server.listen(3000,'127.0.0.1',()=>{
-    console.log('Server running at port 3000')
+clients = 0
+io.on('connection',(socket)=>{
+    clients++;
+    socket.emit('broadcast',{description: clients + 'connected'});
+    socket.on('disconnect',()=>{
+        clients--;
+        socket.broadcast.emit('broadcast',{description: clients + 'disconnected'});
+    })
 })
+
+// io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
